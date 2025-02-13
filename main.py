dd2c43b383d8bdeecaa31eb19b1c9e05b1dbc200
Jotautas Treigys <jotautas.treigys@gmail.com>
2025-02-13 17:49:39 +0200

    now it has state memory

--- main.py
+++ main.py
@@ -15,9 +15,10 @@ from PySide6.QtWidgets import (
     QHBoxLayout,
     QComboBox,
 )
-from PySide6.QtCore import QEvent
+from PySide6.QtCore import Qt, QEvent, QSettings
 from PySide6.QtGui import QStandardItemModel, QStandardItem
 import sys
+import json
 import pandas as pd
 
 from builder import RunlistBuilder
@@ -26,7 +27,12 @@ from builder import RunlistBuilder
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("PySide6 App Layout")
+
+        settings = QSettings("SSAMS", "runlist_builder").value("inputs", "{}")
+        previous_inputs = json.loads(settings)
+        print(previous_inputs)
+
+        self.setWindowTitle("SSAMS Runlist Builder")
         self.setGeometry(100, 100, 800, 600)
 
         # Main layout
@@ -37,11 +43,13 @@ class MainWindow(QWidget):
 
         # Table (Top Left)
         self.table_view = QTableView()
+        self.table_view.setFixedWidth(407)
         self.table_model = QStandardItemModel()
         self.table_view.setModel(self.table_model)
         self.table_view.verticalHeader().setDefaultSectionSize(
             8
-        )  # Set smaller row height
+        )
+        self.table_view  # Set smaller row height
         top_layout.addWidget(self.table_view, 1)
 
         # Text Area (Top Right)
@@ -57,6 +65,7 @@ class MainWindow(QWidget):
 
         # Batch Controls Section
         batch_controls_group = QGroupBox("Batch Controls")
+        batch_controls_group.setFixedWidth(200)
         self.batch_controls_layout = QFormLayout(batch_controls_group)
 
         self.batch_controls_inputs = {
@@ -65,56 +74,103 @@ class MainWindow(QWidget):
             "judge": QCheckBox(),
         }
 
-        self.batch_controls_inputs["mode"].addItems(["nrm", "rpt"])
 
-        for label, widget in self.batch_controls_inputs.items():
+        previous_bc_inputs = previous_inputs.get("batch_controls", {})
+        
+        self.batch_controls_inputs["mode"].addItems(["nrm", "rpt"])
+        previous_mode_input = previous_bc_inputs.get("mode", "nrm")
+        self.batch_controls_inputs["mode"].setCurrentText(previous_mode_input)
+        
+        previous_parkmode_value = previous_bc_inputs.get("parkmode", "off")
+        previous_parkmode_input = True if previous_parkmode_value == "on" else False
+        self.batch_controls_inputs["parkmode"].setChecked(previous_parkmode_input)
+        
+        previous_judge_value = previous_bc_inputs.get("judge", "off")
+        previous_judge_input = True if previous_judge_value == "on" else False
+        self.batch_controls_inputs["judge"].setChecked(previous_judge_input)
+
+        for label_text, widget in self.batch_controls_inputs.items():
+            label = QLabel(label_text)  # Create a QLabel for the setting name
+            label.setFixedWidth(75)
+            widget.setFixedWidth(75)  
             self.batch_controls_layout.addRow(label, widget)
 
         bottom_layout.addWidget(batch_controls_group)
 
         # Settings Section
         settings_group = QGroupBox("Settings")
+        settings_group.setFixedWidth(200)
         self.settings_layout = QFormLayout(settings_group)
 
+        previous_settings_inputs = previous_inputs.get("settings", {})
         self.settings_inputs = {
-            "runs": QLineEdit("20"),
-            "tlimit": QLineEdit("1800"),
-            "jn": QLineEdit("6"),
-            "warm": QLineEdit("100"),
+            "runs": QLineEdit(previous_settings_inputs.get("runs", "20")),
+            "tlimit": QLineEdit(previous_settings_inputs.get("tlimit", "1800")),
+            "jn": QLineEdit(previous_settings_inputs.get("jn", "6")),
+            "warm": QLineEdit(previous_settings_inputs.get("warm", "100")),
         }
-        for label, widget in self.settings_inputs.items():
-            self.settings_layout.addRow(label, widget)
+        # for label, widget in self.settings_inputs.items():
+        #     label.setFixedWidth(70)
+        #     self.settings_layout.addRow(label, widget)
+
+        for label_text, widget in self.settings_inputs.items():
+            label = QLabel(label_text)  # Create a QLabel for the setting name
+            label.setFixedWidth(50)
+            widget.setFixedWidth(75)  # Set the width of the label
+            self.settings_layout.addRow(label, widget)  # Add QLabel and QLineEdit
 
         bottom_layout.addWidget(settings_group)
 
         # JLimits Section
         jlimits_group = QGroupBox("JLimits")
+        jlimits_group.setFixedWidth(200)
         self.jlimits_layout = QVBoxLayout(jlimits_group)
 
+        previous_jlimits_inputs = previous_inputs.get("jlimits", {})
+        grafitas_value, grafitas_checked = previous_jlimits_inputs.get("grafitas", ["9", "off"])
+        ft_value, ft_checked = previous_jlimits_inputs.get("ft", ["3", "off"])
+        c1_value, c1_checked = previous_jlimits_inputs.get("c1", ["0.24", "off"])
+        c2_value, c2_checked = previous_jlimits_inputs.get("c2", ["0.24", "off"])
+        c3_value, c3_checked = previous_jlimits_inputs.get("c3", ["0.23", "off"])
+        c5_value, c5_checked = previous_jlimits_inputs.get("c5", ["0.24", "off"])
+        c7_value, c7_checked = previous_jlimits_inputs.get("c7", ["0.24", "off"])
+        c8_value, c8_checked = previous_jlimits_inputs.get("c8", ["3", "off"])
+        c9_value, c9_checked = previous_jlimits_inputs.get("c9", ["3", "off"])
+        oxii_value, oxii_checked = previous_jlimits_inputs.get("oxii", ["0.5", "off"])
+        
         self.jlimits_inputs = {
-            "default": QLineEdit("0.24"),
-            "grafitas": (QCheckBox(), QLineEdit("9")),
-            "ft": (QCheckBox(), QLineEdit("3")),
-            "c1": (QCheckBox(), QLineEdit("0.24")),
-            "c2": (QCheckBox(), QLineEdit("0.24")),
-            "c3": (QCheckBox(), QLineEdit("0.23")),
-            "c5": (QCheckBox(), QLineEdit("0.24")),
-            "c7": (QCheckBox(), QLineEdit("0.24")),
-            "c8": (QCheckBox(), QLineEdit("3")),
-            "c9": (QCheckBox(), QLineEdit("3")),
-            "oxii": (QCheckBox(), QLineEdit("0.5")),
+            "default": QLineEdit(previous_jlimits_inputs.get("default", "0.24")),
+            "grafitas": (QCheckBox(checked=True if grafitas_checked == "on" else False), QLineEdit(grafitas_value)),
+            "ft": (QCheckBox(checked=True if ft_checked == "on" else False), QLineEdit(ft_value)),
+            "c1": (QCheckBox(checked=True if c1_checked == "on" else False), QLineEdit(c1_value)),
+            "c2": (QCheckBox(checked=True if c2_checked == "on" else False), QLineEdit(c2_value)),
+            "c3": (QCheckBox(checked=True if c3_checked == "on" else False), QLineEdit(c3_value)),
+            "c5": (QCheckBox(checked=True if c5_checked == "on" else False), QLineEdit(c5_value)),
+            "c7": (QCheckBox(checked=True if c7_checked == "on" else False), QLineEdit(c7_value)),
+            "c8": (QCheckBox(checked=True if c8_checked == "on" else False), QLineEdit(c8_value)),
+            "c9": (QCheckBox(checked=True if c9_checked == "on" else False), QLineEdit(c9_value)),
+            "oxii": (QCheckBox(checked=True if oxii_checked == "on" else False), QLineEdit(oxii_value)),
         }
 
-        for label, value in self.jlimits_inputs.items():
+        for label_text, value in self.jlimits_inputs.items():
             row_layout = QHBoxLayout()
+            row_layout.setAlignment(Qt.AlignmentFlag.AlignLeft)
+            label = QLabel(label_text)
+            label.setFixedWidth(50)
             if isinstance(value, tuple):
                 checkbox, line_edit = value
+                line_edit.setFixedWidth(75)
+                checkbox.setFixedWidth(13)
                 row_layout.addWidget(checkbox)
-                row_layout.addWidget(QLabel(label))
+                row_layout.addWidget(label)
                 row_layout.addWidget(line_edit)
             else:
-                row_layout.addWidget(QLabel(label))
+                empty_label = QLabel()
+                empty_label.setFixedWidth(13)
+                row_layout.addWidget(empty_label)
+                row_layout.addWidget(label)
                 row_layout.addWidget(value)
+                value.setFixedWidth(75)
             self.jlimits_layout.addLayout(row_layout)
 
         bottom_layout.addWidget(jlimits_group)
@@ -161,8 +217,12 @@ class MainWindow(QWidget):
             self.populate_table(df)
 
     def populate_table(self, df):
+        df.columns = df.iloc[2]
+        df = df[3:]
         self.table_model.clear()
         self.table_model.setHorizontalHeaderLabels(df.columns.tolist())
+        self.table_view.verticalHeader().setVisible(False)
+        self.table_view.setColumnWidth(0, 30)
         for row in df.itertuples(index=False):
             items = [QStandardItem(str(item)) for item in row]
             self.table_model.appendRow(items)
@@ -188,13 +248,17 @@ class MainWindow(QWidget):
             jlimit_rows.append(items)
         jlimits = {}
         for row in jlimit_rows:
-            if len(row) != 3:
-                jlimits[row[0].widget().text()] = row[1].widget().text()
+            if isinstance(row[0].widget(), QLabel):
+                print("TYPE: 1", type(row[0]), row[0].widget())
+                jlimits[row[1].widget().text()] = row[2].widget().text()
             else:
+                print("TYPE: 2", type(row[0]), row[0].widget())
+                
                 jlimits[row[1].widget().text()] = [
                     row[2].widget().text(),
                     "on" if row[0].widget().isChecked() else "off",
                 ]
+        print(jlimits)
 
 
         settings_widgets = self.settings_layout
@@ -226,10 +290,18 @@ class MainWindow(QWidget):
         batch_controls = dict(bc_pairs)
 
         return jlimits, settings, batch_controls
+    
+    def save_inputs(self) -> None:
+        global_settings = QSettings("SSAMS", "runlist_builder")
+        jlimits, settings, batch_controls = self.get_all_settings()
+        all_settings = {"jlimits": jlimits, "settings": settings, "batch_controls": batch_controls}
+        global_settings.setValue("inputs", json.dumps(all_settings))
+        print("saving inputs..")
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
+    app.aboutToQuit.connect(window.save_inputs)
     sys.exit(app.exec())
